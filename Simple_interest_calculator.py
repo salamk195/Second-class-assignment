0d9e19b33352bc60d42f4367e602c46e4bbdbefe
salamk195 <abdulsalamawosanya5@gmail.com>
2024-06-27 22:11:03 +0100

    Simple_interest_calculator.py

---
+++ Simple_interest_calculator.py
@@ -0,0 +1,6 @@
+# code for calculating simple interest
+principal = int(input ("Enter your principal"))
+rate = int(input ("enter your rate"))
+time =int(input ("enter the time "))
+simple_interest = principal * rate * time 
+print (simple_interest / 100)
