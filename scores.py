8a963f02e4759d7a65baab641a747f75da0ee8c8
salamk195 <abdulsalamawosanya5@gmail.com>
2024-07-01 12:08:53 +0100

    scores.py

---
+++ scores.py
@@ -0,0 +1,24 @@
+Total_amount_of_goods_bought = ("Enter the total amount of goods you bought")
+if Total_amount_of_goods_bought > 1000 < 2000:
+elif user_inputed_value == R :
+   print( Total_amount_of_goods_bought * 5 / 100 )
+elif user_inputed_value = S:
+    print( Total_amount_of_goods_bought * 10 / 100)
+elif user_inputed_value = G :
+    print(Total_amount_of_goods_bought * 15 / 100 )
+elif user_inputed_value = P :
+    print(Total_amount_of_goods_bought * 20 / 100)
+
+
+
+if Total_amount_of_goods_bought > 2000 :
+elif user_inputed_value == R :
+   print( Total_amount_of_goods_bought * 15 / 100 )
+elif user_inputed_value = S:
+    print( Total_amount_of_goods_bought * 20 / 100)
+elif user_inputed_value = G :
+    print(Total_amount_of_goods_bought * 25 / 100 )
+elif user_inputed_value = P :
+    print(Total_amount_of_goods_bought * 30 / 100)
+    
+
